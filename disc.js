f2db625dc58756dbdd594729923d618a4fb9ad60
Blaine Hurt <d.blaine.hurt@gmail.com>
2018-10-11 22:21:52 -0500

    dominant trait added to email

--- disc.js
+++ disc.js
@@ -1,3 +1,20 @@
+var dominant = function(type){
+	if(type == "D"){
+		return "D's are direct and decisive; they are risk takers and problem solvers. They are more concerned with completing tasks and winning than they are with gaining approval from people. Though the internal drive tends to make them insensitive to those around them, D's are not afraid to challenge the status quo, and they thrive when it comes to developing new things. They need discipline to excel, and they respond to direct confrontation. The greatest fear of a “D” is to be taken advantage of, and even despite their possible weaknesses—which include an aversion to routine, a tendency to overstep authority, an argumentative nature, and a habit of taking on too much—they place a high value on time and use their innovative thinking to accomplish difficult tasks and conquer challenges.";
+	}
+	else if(type == "I"){
+		return "I's are inspiring and impressive. Enthusiastic, optimistic, impulsive, and emotional—they tend to be creative problem solvers and excellent encouragers. They often have a large number of friends, but they can become more concerned with approval and popularity than with getting results. An I's greatest fear is rejection, but they thrive when it comes to motivating others. Their positive sense of humor helps them negotiate conflicts. Though they can be inattentive to details and poor listeners, they can be great peacemakers and effective teammates when they control their feelings and minimize their urge to entertain and be the center of attention. They value lots of human touch and connection.";
+	}
+	else if(type == "S"){
+		return "S's are steady and more reserved. Because they are stable and predictable, they do not like change, and they thrive in secure, non-threatening environments. They are often friendly and understanding as well as good listeners and loyal workers who are happy doing the same job consistently. With an incredible ability to forgive, reliable and dependable S's tend to make the best friends. Their greatest fear, however, is loss of security, and their possible weaknesses naturally include not only resistance to change, but also difficulty adjusting to it. They can also be too sensitive to criticism and unable to establish priorities. In order to avoid be taken advantage of, S's need to be stronger and learn how to say “no.” They also like to avoid the limelight, but when given an opportunity to genuinely help others, they will gladly rise to the occasion. They feel most valued when they have truly helped someone.";
+	}
+	else if(type == "C"){
+		return "C's are compliant and analytical. Careful and logical lines of thinking drive them forward, and accuracy is a top priority. They hold high standards and value systematic approaches to problem solving. Though they thrive when given opportunities to find solutions, they tend to ignore the feelings of others and can often be critical and downright crabby. Verbalizing feelings is difficult for them, but when they are not bogged down in details and have clear-cut boundaries, they can be big assets to the team by providing calculated “reality checks.” The C's biggest fear is criticism, and their need for perfectionism is often a weakness, as is their tendency to give in when in the midst of an argument. However, they are thorough in all activities and can bring a conscientious, even-tempered element to the team that will provide solid grounding.They value being correct the most.";
+	}
+	else{
+		return "";
+	}
+}
 var obj = function(type,score){
 	this.type = type;
 	this.score = score;
@@ -184,9 +201,13 @@ var TestViewModel = function(){
 		
 	}, this);
 	
+	/*_self.message = ko.pureComputed(function(){
+		return "test\ntest1";
+	},this*/
 	_self.message = ko.pureComputed(function(){
-		var mes = _self.Traits()[0].type + _self.Traits()[0].score.toString() + _self.Traits()[1].type + _self.Traits()[1].score.toString();
-		return mes;
+		var T1 = _self.Traits()[0].type;
+		//var mes = _self.Traits()[0].type + _self.Traits()[0].score.toString() + _self.Traits()[1].type + _self.Traits()[1].score.toString();
+		return "Your dominant trait is: " + T1 + "!\\\n\\\n" + dominant(T1);
 	},this);
 	
 	
