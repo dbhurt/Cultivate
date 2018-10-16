e5e4f59c47fde3c875614000d9299d3be48fb527
Blaine Hurt <d.blaine.hurt@gmail.com>
2018-10-15 22:07:52 -0500

    gifts results are styled
    gifts results are styled

--- gifts.js
+++ gifts.js
@@ -1,3 +1,20 @@
+var Gift = function(name, score, info){
+	var _self = this;
+	_self.name = name || "";
+	_self.score = score;
+	_self.info = info;
+	_self.statusClass = ko.pureComputed(function(){
+		if(_self.score() <= 15 && _self.score() >10){
+			return 'green';
+		}
+		
+		if(_self.score() <= 10 && _self.score() > 5){
+			return 'yellow';
+		}
+		
+		return 'red';
+	});
+};
 
 jQuery.urlParam = function (name) {
     var results = new RegExp('[\?&]' + name + '=([^&#]*)')
@@ -501,30 +518,30 @@ var GiftsViewModel = function(testing){
 	});
 	
 	_self.GiftsList = ko.observableArray([
-		{ name: "Administration", score: _self.Administration(), info: "The gift of administration is the divine strength or ability to organize multiple tasks and groups of people to accomplish these tasks. Luke 14:28-30, Acts 6:1-7, 1 Corinthians 12:28" },
-		{ name: "Apostleship", score: _self.Apostleship(), info: "The gift of apostleship is the divine strength or ability to pioneer new churches and ministries through planting, overseeing, and training. Acts 15:22-35, 1 Corinthians 12:28, 2 Corinthians 12:12, Gal 2:7-10, Ephesians 4:11-14" },
-		{ name: "Craftsmanship", score: _self.Craftsmanship(), info: "The gift of craftsmanship is the divine strength or ability to plan, build, and work with your hands in construction environments to accomplish multiple ministry applications. Exodus 30:22, 31:3-11, 2 Chronicles 34:9-13, Acts 18:2-3"},
-		{ name: "Discernment", score: _self.Discernment(), info: "The gift of discernment is the divine strength or ability to spiritually identify falsehood and to distinguish between right and wrong motives and situations. Matt. 16:21-23, Acts 5:1-11, 16:16-18, I Cor. 12:10, I John 4:1-6" },
-		{ name: "Evangelism", score: _self.Evangelism(), info: "The gift of evangelism is the divine strength or ability to help non-Christians take the necessary steps to becoming a born again Christian. Acts 8:5-6, Acts 8:26-40, Acts 14:21, Acts 21:8, Ephesians 4:11-14" },
-		{ name: "Exhortation", score: _self.Exhortation(), info: "The gift of exhortation is the divine strength or ability to encourage others through the written or spoken word and Biblical truth. Acts 14:22, Romans 12:8, 1 Timothy 4:13, Hebrews 10:24-25" },
-		{ name: "Faith", score: _self.Faith(), info: "The gift of faith is the divine strength or ability to believe in God for unseen supernatural results in every arena of life. Acts 11:22-24, Rom 4:18-21, 1 Corinthians 12:9, Hebrews 11" },
-		{ name: "Giving", score: _self.Giving(), info: "The gift of giving is the divine strength or ability to produce wealth and to give by tithes and offerings for the purpose of advancing the Kingdom of God on earth. Mark 12:41-44, Romans 12:8, 2 Corinthians 8:1-7, 9:2-7" },
-		{ name: "Healing", score: _self.Healing(), info: "The gift of healing is the divine strength or ability to act as an intermediary in faith,prayer, & by the laying on of hands for the healing of physical and mental illnesses. Acts 3:1-10, Acts 9:32-35, Acts 28:7-10, 1 Corinthians 12:9, 28" },
-		{ name: "Helps", score: _self.Helps(), info: "The gift of helps is the divine strength or ability to work in a supportive role for the accomplishment of tasks in Christian ministry. Mark 15:40-41, Acts 9:36, Romans 16:1-2, 1 Corinthians 12:28" },
-		{ name: "Hospitality", score: _self.Hospitality(), info: "The gift of hospitality is the divine strength or ability to create warm, welcoming environments for others in places such as your home, office, or church. Acts 16:14-15, Romans 12:13, Romans 16:23, Hebrews 13:1-2, 1 Peter 4:9" },
-		{ name: "Intercession", score: _self.Intercession(), info: "The gift of intercession is the divine strength or ability to stand in the gap in prayer for someone, something, or someplace believing for profound results. Hebrews 7:25, Colossians 1:9-12, Col 4:12-13, James 5:14-16" },
-		{ name: "Knowledge", score: _self.Knowledge(), info: "The gift of knowledge is the divine strength or ability to bring clarity and to understand situations and circumstances often accompanied by a word from God. Acts 5:1-11, 1 Corinthians 12:8, Col 2:2-3" },
-		{ name: "Leadership", score: _self.Leadership(), info: "The gift of leadership is the divine strength or ability to influence people at their level while directing and focusing them on the big picture, vision, or idea. Romans 12:8, 1 Timothy 3:1-13, 1 Timothy 5:17, Hebrews 13:17" },
-		{ name: "Mercy", score: _self.Mercy(), info: "The gift of mercy is the divine strength or ability to feel empathy and to care for those who are hurting in any way. Matthew 9:35-36, Mark 9:41, Romans 12:8, 1 Thessalonians 5:14" },
-		{ name: "Miracles", score: _self.Miracles(), info: "The gift of miracles is the divine strength or ability to alter the natural outcomes of life in a supernatural way through prayer, faith, and divine direction. Acts 9:36-42, 19:11-12, 20:7-12, Rom 15:18-19, I Cor. 12:10, 28" },
-		{ name: "Missionary", score: _self.Missionary(), info: "The gift of missions is the divine strength or ability to reach others outside of your culture and nationality; while in most cases living in that culture or nation. Acts 8:4, Acts 13:2-3, Acts 22:21, Romans 10:15" },
-		{ name: "MusicWorship", score: _self.MusicWorship(), info: "The gift of music / worship is the divine strength or ability to sing, dance, or play an instrument primarily for the purpose of helping others worship God. Deut. 31:22, 1 Samuel 16:16, 1 Chronicles 16:41-42, 2 Chronicles 5:12-13, 34:12, Psalm 150" },
-		{ name: "PastorShepherd", score: _self.PastorShepherd(), info: "The gift of pastor / shepherd is the divine strength or ability to care for the personal needs of others by nurturing and mending life issues. John 10:1-18, Ephesians 4:11-14, 1 Timothy 3:1-7, 1 Peter 5:1-3" },
-		{ name: "Prophecy", score: _self.Prophecy(), info: "The gift of prophecy is the divine strength or ability to boldly speak and bring clarity to scriptural and doctrinal truth, in some cases foretelling God’s plan. Acts 2:37-40, Acts 7:51-53, Acts 26:24-29, 1 Corinthians 14:1-4, 1 Thessalonians. 1:5" },
-		{ name: "Service", score: _self.Service(), info: "The gift of serving is the divine strength or ability to do small or great tasks in working for the overall good of the body of Christ. Acts 6:1-7, Romans 12:7, Galatians 6:10, 2 Tim. 1:16-18, Titus 3:14" },
-		{ name: "Teaching", score: _self.Teaching(), info: "The gift of teaching is the divine strength or ability to study and learn from the Scriptures primarily to bring understanding and depth to other Christians. Acts 18:24-28, Acts 20:20-21, 1 Corinthians. 12:28, Ephesians 4:11-14" },
-		{ name: "Tongues", score: _self.Tongues(), info: "The gift of tongues is the divine strength or ability to pray in a heavenly language to encourage your spirit and to commune with God. The gift of tongues is often accompanied by interpretation and should be used appropriately. Acts 2:1-13, 1 Corinthians 12:10, 14:1-14" },
-		{ name: "Wisdom", score: _self.Wisdom(), info: "The gift of wisdom is the divine strength or ability to apply the truths of Scripture in a practical way, producing the fruitful outcome and character of Jesus Christ. Acts 6:3,10, 1 Corinthians 2:6-13, 1 Corinthians. 12:8" }
+		new Gift("Administration",_self.Administration,"The gift of administration is the divine strength or ability to organize multiple tasks and groups of people to accomplish these tasks. Luke 14:28-30, Acts 6:1-7, 1 Corinthians 12:28"),
+		new Gift("Apostleship", _self.Apostleship, "The gift of apostleship is the divine strength or ability to pioneer new churches and ministries through planting, overseeing, and training. Acts 15:22-35, 1 Corinthians 12:28, 2 Corinthians 12:12, Gal 2:7-10, Ephesians 4:11-14"),
+		new Gift("Craftsmanship", _self.Craftsmanship, "The gift of craftsmanship is the divine strength or ability to plan, build, and work with your hands in construction environments to accomplish multiple ministry applications. Exodus 30:22, 31:3-11, 2 Chronicles 34:9-13, Acts 18:2-3"),
+		new Gift("Discernment", _self.Discernment, "The gift of discernment is the divine strength or ability to spiritually identify falsehood and to distinguish between right and wrong motives and situations. Matt. 16:21-23, Acts 5:1-11, 16:16-18, I Cor. 12:10, I John 4:1-6"),
+		new Gift("Evangelism", _self.Evangelism, "The gift of evangelism is the divine strength or ability to help non-Christians take the necessary steps to becoming a born again Christian. Acts 8:5-6, Acts 8:26-40, Acts 14:21, Acts 21:8, Ephesians 4:11-14"),
+		new Gift("Exhortation", _self.Exhortation, "The gift of exhortation is the divine strength or ability to encourage others through the written or spoken word and Biblical truth. Acts 14:22, Romans 12:8, 1 Timothy 4:13, Hebrews 10:24-25"),
+		new Gift("Faith", _self.Faith, "The gift of faith is the divine strength or ability to believe in God for unseen supernatural results in every arena of life. Acts 11:22-24, Rom 4:18-21, 1 Corinthians 12:9, Hebrews 11"),
+		new Gift("Giving", _self.Giving, "The gift of giving is the divine strength or ability to produce wealth and to give by tithes and offerings for the purpose of advancing the Kingdom of God on earth. Mark 12:41-44, Romans 12:8, 2 Corinthians 8:1-7, 9:2-7"),
+		new Gift("Healing", _self.Healing, "The gift of healing is the divine strength or ability to act as an intermediary in faith,prayer, & by the laying on of hands for the healing of physical and mental illnesses. Acts 3:1-10, Acts 9:32-35, Acts 28:7-10, 1 Corinthians 12:9, 28"),
+		new Gift("Helps", _self.Helps, "The gift of helps is the divine strength or ability to work in a supportive role for the accomplishment of tasks in Christian ministry. Mark 15:40-41, Acts 9:36, Romans 16:1-2, 1 Corinthians 12:28"),
+		new Gift("Hospitality", _self.Hospitality, "The gift of hospitality is the divine strength or ability to create warm, welcoming environments for others in places such as your home, office, or church. Acts 16:14-15, Romans 12:13, Romans 16:23, Hebrews 13:1-2, 1 Peter 4:9"),
+		new Gift("Intercession", _self.Intercession, "The gift of intercession is the divine strength or ability to stand in the gap in prayer for someone, something, or someplace believing for profound results. Hebrews 7:25, Colossians 1:9-12, Col 4:12-13, James 5:14-16"),
+		new Gift("Knowledge", _self.Knowledge, "The gift of knowledge is the divine strength or ability to bring clarity and to understand situations and circumstances often accompanied by a word from God. Acts 5:1-11, 1 Corinthians 12:8, Col 2:2-3"),
+		new Gift("Leadership", _self.Leadership, "The gift of leadership is the divine strength or ability to influence people at their level while directing and focusing them on the big picture, vision, or idea. Romans 12:8, 1 Timothy 3:1-13, 1 Timothy 5:17, Hebrews 13:17"),
+		new Gift("Mercy", _self.Mercy, "The gift of mercy is the divine strength or ability to feel empathy and to care for those who are hurting in any way. Matthew 9:35-36, Mark 9:41, Romans 12:8, 1 Thessalonians 5:14"),
+		new Gift("Miracles", _self.Miracles, "The gift of miracles is the divine strength or ability to alter the natural outcomes of life in a supernatural way through prayer, faith, and divine direction. Acts 9:36-42, 19:11-12, 20:7-12, Rom 15:18-19, I Cor. 12:10, 28"),
+		new Gift("Missionary", _self.Missionary, "The gift of missions is the divine strength or ability to reach others outside of your culture and nationality; while in most cases living in that culture or nation. Acts 8:4, Acts 13:2-3, Acts 22:21, Romans 10:15"),
+		new Gift("MusicWorship", _self.MusicWorship, "The gift of music / worship is the divine strength or ability to sing, dance, or play an instrument primarily for the purpose of helping others worship God. Deut. 31:22, 1 Samuel 16:16, 1 Chronicles 16:41-42, 2 Chronicles 5:12-13, 34:12, Psalm 150"),
+		new Gift("PastorShepherd", _self.PastorShepherd, "The gift of pastor / shepherd is the divine strength or ability to care for the personal needs of others by nurturing and mending life issues. John 10:1-18, Ephesians 4:11-14, 1 Timothy 3:1-7, 1 Peter 5:1-3"),
+		new Gift("Prophecy", _self.Prophecy, "The gift of prophecy is the divine strength or ability to boldly speak and bring clarity to scriptural and doctrinal truth, in some cases foretelling God’s plan. Acts 2:37-40, Acts 7:51-53, Acts 26:24-29, 1 Corinthians 14:1-4, 1 Thessalonians. 1:5"),
+		new Gift("Service", _self.Service, "The gift of serving is the divine strength or ability to do small or great tasks in working for the overall good of the body of Christ. Acts 6:1-7, Romans 12:7, Galatians 6:10, 2 Tim. 1:16-18, Titus 3:14"),
+		new Gift("Teaching", _self.Teaching, "The gift of teaching is the divine strength or ability to study and learn from the Scriptures primarily to bring understanding and depth to other Christians. Acts 18:24-28, Acts 20:20-21, 1 Corinthians. 12:28, Ephesians 4:11-14"),
+		new Gift("Tongues", _self.Tongues, "The gift of tongues is the divine strength or ability to pray in a heavenly language to encourage your spirit and to commune with God. The gift of tongues is often accompanied by interpretation and should be used appropriately. Acts 2:1-13, 1 Corinthians 12:10, 14:1-14"),
+		new Gift("Wisdom", _self.Wisdom, "The gift of wisdom is the divine strength or ability to apply the truths of Scripture in a practical way, producing the fruitful outcome and character of Jesus Christ. Acts 6:3,10, 1 Corinthians 2:6-13, 1 Corinthians. 12:8")
 	]);
 	
 	
